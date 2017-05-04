19948b4e16d934c2fe271a788b1e165472b328ed
Niclas Krabbe Kristensen <krabbe89@gmail.com>
2017-05-04 11:27:17 +0200

    Created propertys, conctructor and tostring

--- Projekt/Model/Product.cs
+++ Projekt/Model/Product.cs
@@ -8,5 +8,79 @@ namespace Projekt.Model
 {
     class Product
     {
+        private String _name;
+        private String _content;
+        private double _price;
+        private int _countMade;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return _content;
+            }
+
+            set
+            {
+                _content = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                _price = value;
+            }
+        }
+
+        public int CountMade
+        {
+            get
+            {
+                return _countMade;
+            }
+
+            set
+            {
+                _countMade = value;
+            }
+        }
+
+        public Product()
+        {
+            
+        }
+
+        public Product(string name, string content, double price, int countMade)
+        {
+            _name = name;
+            _content = content;
+            _price = price;
+            _countMade = countMade;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Content)}: {Content}, {nameof(Price)}: {Price}, {nameof(CountMade)}: {CountMade}";
+        }
     }
 }
