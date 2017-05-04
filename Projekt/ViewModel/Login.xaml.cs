459a9a7bbba1060b9394fc81bea34b462e085d81
Xentrosh <fabianwendell@hotmail.com>
2017-05-04 09:36:56 +0200

    LoginFix

--- Projekt/ViewModel/Login.xaml.cs
+++ Projekt/ViewModel/Login.xaml.cs
@@ -20,9 +20,9 @@ namespace Projekt
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
             this.InitializeComponent();
         }
